9876197ce08832bd70959df3de4aee56fce5a9cd
Manuel Schmidt <manuel.schmidt@7p-group.com>
2016-05-03 09:55:18 +0200

    Implementation of Simon says by Clara and Pia

--- app/src/main/java/simonsays/sevenprinciples/com/simonsays/Color.java
+++ app/src/main/java/simonsays/sevenprinciples/com/simonsays/Color.java
@@ -4,10 +4,10 @@ package simonsays.sevenprinciples.com.simonsays;
  * Created by manuel on 26.04.16.
  */
 public enum Color {
-    RED('r', R.raw.red),
-    GREEN('g', R.raw.green),
-    YELLOW('y', R.raw.yellow),
-    BLUE('b', R.raw.blue);
+    RED('r', R.raw.clara_pia_rot),
+    GREEN('g', R.raw.clara_pia_gruen),
+    YELLOW('y', R.raw.clara_pia_gelb),
+    BLUE('b', R.raw.clara_pia_blau);
 
     private char firstLetter;
     private int soundFileResource;
--- app/src/main/java/simonsays/sevenprinciples/com/simonsays/MainActivity.java
+++ app/src/main/java/simonsays/sevenprinciples/com/simonsays/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,10 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     // Buttons
     // ToDo: define and initizialise buttons
+    @Bind(R.id.RotButton)
+    Button Grün;
 
     private StringBuffer colorsOfPlayer1;
     private StringBuffer colorsOfPlayer2;
 
+    @OnClick (R.id.RotButton)
+    protected void Nutella(View view) {
+        String message = "Hallöchen";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,34 +47,42 @@ public class MainActivity extends AppCompatActivity {
         //startNewGame();
     }
 
-//    // click on red, green, yellow, blue button
-//    @OnClick({R.id.redButton, R.id.greenButton, R.id.yellowButton, R.id.blueButton})
-//    protected void onCickButton(View view) {
-//        Log.d(TAG, view.getTag() + " button was clicked");
-//
-//        // get current color and play sound of them
-//        Color currentColor = Color.valueOf(view.getTag().toString());
-//        Sound.playSound(getApplicationContext(), currentColor.getSoundFileResource());
-//
-//        gameLogic();
-//    }
+    // click on red, green, yellow, blue button
+    @OnClick({R.id.RotButton, R.id.GrünButton, R.id.GelbButton, R.id.BlauButton})
+    protected void onCickButton(View view) {
+        Log.d(TAG, view.getTag() + " button was clicked");
 
-    private void gameLogic() {
-        // ToDo
+        // get current color and play sound of them
+        Color currentColor = Color.valueOf(view.getTag().toString());
+        Sound.playSound(getApplicationContext(), currentColor.getSoundFileResource());
+
+        gameLogic();
     }
 
-//    // click on new game button
-//    @OnClick(R.id.newGameButton)
-//    protected void clickOnNewGameButton(View view) {
-//        Log.d(TAG, "start new game");
-//        startNewGame();
-//    }
-//
-//    private void startNewGame() {
-//        colorsOfPlayer1 = new StringBuffer();
-//        colorsOfPlayer2 = new StringBuffer();
-//        playerLabelText.setText(R.string.player1);
-//    }
+    private void gameLogic(Color currentColor) {
+        if(colorsOfPlayer1.length() == 4){
+            colorsOfPlayer2.append(currentColor.getFirstLetter());
+            if (colorsOfPlayer2.length()==4){
+                checkTheResult();
+                Print print = new Print (getApplicationContext());
+                print.printChooseOfColors(colorsOfPlayer1, colorsOfPlayer2);
+                startNewGame();
+            }
+        }
+    }
+
+    // click on new game button
+    @OnClick(R.id.newGameButton)
+    protected void clickOnNewGameButton(View view) {
+        Log.d(TAG, "start new game");
+        startNewGame();
+    }
+
+    private void startNewGame() {
+        colorsOfPlayer1 = new StringBuffer();
+        colorsOfPlayer2 = new StringBuffer();
+        playerLabelText.setText(R.string.player1);
+    }
 
     private void checkTheResult() {
         // Todo

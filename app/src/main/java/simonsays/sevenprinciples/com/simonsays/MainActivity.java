e8881a5b5b490f628100d1f29fecf4722170cc18
Manuel Schmidt <manuel.schmidt@7p-group.com>
2016-05-06 08:59:00 +0200

    add missing code

--- app/src/main/java/simonsays/sevenprinciples/com/simonsays/MainActivity.java
+++ app/src/main/java/simonsays/sevenprinciples/com/simonsays/MainActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,26 +16,22 @@ import butterknife.OnClick;
 /**
  * Simon Says
  * <p>
- * Created by Manuel Schmidt on 28/04/2016.
+ * Created by Manuel Schmidt, Pia Rosenbuam, Clara Hartmann on 28/04/2016.
  * Copyright © 2016 Manuel Schmidt. All rights reserved.
  */
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
-    // Buttons
-    // ToDo: define and initizialise buttons
-    @Bind(R.id.RotButton)
-    Button Grün;
-
     private StringBuffer colorsOfPlayer1;
     private StringBuffer colorsOfPlayer2;
 
-    @OnClick (R.id.RotButton)
-    protected void Nutella(View view) {
-        String message = "Hallöchen";
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-    }
+    // Text
+    @Bind(R.id.playerLabel) TextView playerLabelText;
+
+    // Buttons
+    @Bind(R.id.RotButton)
+    Button Grün;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        //startNewGame();
+        startNewGame();
     }
 
     // click on red, green, yellow, blue button
@@ -56,26 +51,28 @@ public class MainActivity extends AppCompatActivity {
         Color currentColor = Color.valueOf(view.getTag().toString());
         Sound.playSound(getApplicationContext(), currentColor.getSoundFileResource());
 
-        gameLogic();
+        gameLogic(currentColor);
     }
 
     private void gameLogic(Color currentColor) {
-        if(colorsOfPlayer1.length() == 4){
+        if (colorsOfPlayer1.length() == 4) {
             colorsOfPlayer2.append(currentColor.getFirstLetter());
-            if (colorsOfPlayer2.length()==4){
+
+            if (colorsOfPlayer2.length() == 4) {
                 checkTheResult();
-                Print print = new Print (getApplicationContext());
+
+                // print results
+                Print print = new Print(getApplicationContext());
                 print.printChooseOfColors(colorsOfPlayer1, colorsOfPlayer2);
+
                 startNewGame();
             }
+        } else if (colorsOfPlayer2.length() == 0) {
+            if (colorsOfPlayer1.length() == 3) {
+                playerLabelText.setText(R.string.player2);
+            }
+            colorsOfPlayer1.append(currentColor.getFirstLetter());
         }
-    }
-
-    // click on new game button
-    @OnClick(R.id.newGameButton)
-    protected void clickOnNewGameButton(View view) {
-        Log.d(TAG, "start new game");
-        startNewGame();
     }
 
     private void startNewGame() {
@@ -85,7 +82,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkTheResult() {
-        // Todo
+        if (colorsOfPlayer1.toString().equals(colorsOfPlayer2.toString())) {
+            Sound.playSound(getApplicationContext(), R.raw.clara_pia_win);
+            showDialog(getString(R.string.right), android.R.drawable.ic_dialog_info);
+
+        } else {
+            Sound.playSound(getApplicationContext(), R.raw.clara_pia_lose);
+            showDialog(getString(R.string.fail), android.R.drawable.ic_dialog_alert);
+        }
     }
 
     private void showDialog(final String messageText, final int icon) {
@@ -94,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 .setMessage(messageText)
                 .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        /* startNewGame(); */
+                        startNewGame();
                     }
                 })
                 .setIcon(icon)
